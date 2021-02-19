d6cd69605b0aa4b4ed2132cc828634e9afdedf23
David Morais <davidmorais92@gmail.com>
2021-02-19 22:28:47 +0000

    add config file

---
+++ src/config.js
@@ -0,0 +1,24 @@
+export default {
+  title: "Hello\nI'm David",
+  subtitle: "And I'm a full stack web developer",
+  skills: [
+    'React',
+    'JavaScript',
+    'CSS',
+    'Node JS',
+    'Electron',
+    'Ruby on Rails',
+    'WordPress',
+    'D3',
+    'React Native',
+  ],
+  contacts: {
+    phone: '+351 934 645 727',
+    email: 'david@dsmorais.com',
+  },
+  socials: {
+    twitter: 'davidsmorais',
+    telegram: '@davidsmorais',
+    devTo: 'dsmorais',
+  }
+};
