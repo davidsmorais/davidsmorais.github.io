229625e5bb390d619bd639b189650000a2b6e996
David Morais <davidmorais92@gmail.com>
2021-02-22 19:38:41 +0000

    style input and text area

--- src/common/Input/styles.js
+++ src/common/Input/styles.js
@@ -4,22 +4,41 @@ export const Container = styled.div`
   display: inline-block;
   width: 100%;
   padding: 10px 5px;
+  label {
+    font-family: Raleway;
+    color: ${({theme}) => theme.accent.secondary}
+  }
 `;
 
 export const Input = styled.input`
   width: 100%;
   border-width: 1px;
   border-style: solid;
-  border-color: -internal-light-dark(rgb(118, 118, 118), rgb(133, 133, 133));
+  border-color: ${({ theme }) => theme.accent.secondary};
   outline: none;
   font-size: 0.875rem;
   padding: 1rem 1.25rem;
   transition: border-color 0.3s ease-in;
   border-radius: 8px;
-  color: #000;
+  color: ${({ theme }) => theme.accent.main};
+  font-family: Cabin;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 125%;
+  background: ${({ theme }) => theme.background};
+  border-radius: 0;
+  &::placeholder {
+    font-family: Cabin;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 150%;
+    color: ${({ theme }) => theme.accent.secondary};
+  }
 
   &:focus,
   &:hover {
-    border-color: #2e186a;
+    border-color: ${({ theme }) => theme.accent.main};
   }
 `;
--- src/common/TextArea/styles.js
+++ src/common/TextArea/styles.js
@@ -5,6 +5,10 @@ export const Container = styled.div`
   width: 100%;
   padding: 10px 5px;
   margin-bottom: -0.625rem;
+  label {
+    font-family: Raleway;
+    color: ${({ theme }) => theme.accent.secondary};
+  }
 `;
 
 export const TextArea = styled.textarea`
@@ -16,9 +20,26 @@ export const TextArea = styled.textarea`
   font-size: 0.875rem;
   height: 185px;
   transition: border-color 0.3s ease-in;
+  color: ${({ theme }) => theme.accent.main};
+  font-family: Cabin;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 125%;
+  background: ${({ theme }) => theme.background};
+  border-color: ${({ theme }) => theme.accent.secondary};
+  border-radius: 0;
+  &::placeholder {
+    font-family: Cabin;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 150%;
+    color: ${({ theme }) => theme.accent.secondary};
+  }
 
   &:focus,
   &:hover {
-    border-color: #2e186a;
+    border-color: ${({ theme }) => theme.accent.main};
   }
 `;
