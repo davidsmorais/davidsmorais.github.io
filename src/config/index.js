788f9f5b5aff58c40d6651b006a9d56ab03b9151
David Morais <davidmorais92@gmail.com>
2021-02-25 00:25:28 +0000

    add terminal config

---
+++ src/config/index.js
@@ -0,0 +1,28 @@
+import skillsTerminal from './skillsTerminal'
+
+export default {
+  title: "Hello\nI'm David",
+  subtitle: "And I'm a web developer",
+  skills: [
+    "React",
+    "JavaScript",
+    "CSS",
+    "Node JS",
+    "Electron",
+    "Ruby on Rails",
+    "WordPress",
+    "D3",
+    "React Native",
+  ],
+  contacts: {
+    phone: "+351 934 645 727",
+    email: "david@dsmorais.com",
+  },
+  socials: {
+    twitter: "davidsmorais",
+    telegram: "@davidsmorais",
+    devTo: "dsmorais",
+    github: "davidsmorais",
+  },
+  skillsTerminal,
+};
