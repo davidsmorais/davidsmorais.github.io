e9ce825de7c0e3e81e5baf24de8a74df9b802ed0
David Morais <davidmorais92@gmail.com>
2021-02-26 21:10:20 +0000

    🐛 FIX: layout

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,91 +2,99 @@ import { lazy } from "react";
 import styled from "styled-components";
 
 import IntroContent from "../../content/IntroContent.json";
-import MiddleBlockContent from "../../content/MiddleBlockContent.json";
 import AboutContent from "../../content/AboutContent.json";
-// import MissionContent from "../../content/MissionContent.json";
-import ProductContent from "../../content/ProductContent.json";
 import ContactContent from "../../content/ContactContent.json";
-import CONFIG from 'Config';
+import CONFIG from "Config";
+import { Row } from "Common";
 
 const SkillsTerminal = lazy(() => import("Components/SkillsTerminal"));
 const ContactFrom = lazy(() => import("Components/ContactForm"));
 const ContentBlock = lazy(() => import("Components/ContentBlock"));
-const MiddleBlock = lazy(() => import("Components/MiddleBlock"));
+// const MiddleBlock = lazy(() => import("Components/MiddleBlock"));
 const ScrollToTop = lazy(() => import("Common/ScrollToTop"));
 const Container = lazy(() => import("Common/Container"));
 
 const StyledBackgroundContainer = styled(Container)`
   &&& {
-    background-image: linear-gradient(
-        180deg,
-        #b3f9e680 0%, rgba(18, 18, 18, 0.5) 100%
-      ),
+    background-image: linear-gradient(180deg, #3a907880 0%, rgb(0 0 0) 100%),
       url("/img/bg1.png");
     background-attachment: fixed;
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
-    background-blend-mode: hard-light;
+    position: relative;
+    top: -50px;
+    max-width: unset;
+    > div {
+      padding: ${({ theme }) => theme.margin * 5}px;
+    }
+  }
+`;
+
+const StyledContainer = styled(Container)`
+  position: relative;
+  top: -50px;
+  max-width: unset;
+  padding: 0;
+  > div,
+  section {
+    padding: ${({ theme }) => theme.margin * 5}px;
   }
 `;
 
 const StyledBlackAndWhiteContainer = styled(Container)`
-&&& {
-  height: 150%;
-  background: linear-gradient(
-    180deg,
-    ${({ theme }) => theme.accent.secondary} 50%,
-    ${({ theme }) => theme.background} 50%
-  );
-}
+  &&& {
+    height: 150%;
+    padding-top: 15%;
+    max-width: unset;
+    background: linear-gradient(
+      180deg,
+      ${({ theme }) => theme.accent.secondary} 50%,
+      ${({ theme }) => theme.background} 50%
+    );
+  }
 `;
 
 const Home = () => {
   return (
-    <Container>
+    <StyledContainer>
       <ScrollToTop />
       <StyledBackgroundContainer>
-      <ContentBlock
-        type="right"
-        first="true"
-        title={IntroContent.title}
-        content={IntroContent.text}
-        button={IntroContent.button}
-        icon="avatar.png"
-        id="intro"
-      />
+        <Row justify="center">
+          <ContentBlock
+            type="right"
+            first="true"
+            title={IntroContent.title}
+            subtitle={IntroContent.text}
+            button={IntroContent.button}
+            icon="avatar.png"
+            id="intro"
+          />
+        </Row>
       </StyledBackgroundContainer>
-      <MiddleBlock
-        title={MiddleBlockContent.title}
-        content={MiddleBlockContent.text}
-        button={MiddleBlockContent.button}
-      />
-      <ContentBlock
-        type="left"
-        title={AboutContent.title}
-        content={AboutContent.text}
-        section={AboutContent.section}
-        icon="graphs.svg"
-        id="about"
-      />
-      <SkillsTerminal skills={CONFIG.skillsTerminal}/>
 
-      <ContentBlock
-        type="left"
-        title={ProductContent.title}
-        content={ProductContent.text}
-        icon="waving.svg"
-        id="product"
-      />
+      <Container>
+        <ContentBlock
+          type="left"
+          title={"> blog"}
+          content={AboutContent.text}
+          section={AboutContent.section}
+          id="about"
+        />
+      </Container>
+      <Container>
+        <SkillsTerminal skills={CONFIG.skillsTerminal} />
+      </Container>
       <StyledBlackAndWhiteContainer>
-      <ContactFrom
-        title={ContactContent.title}
-        content={ContactContent.text}
-        id="contact"
-      />
+        <Row justify="center">
+          <ContactFrom
+            title={ContactContent.title}
+            content={ContactContent.subtitle}
+            id="contact"
+          />
+        </Row>
       </StyledBlackAndWhiteContainer>
-    </Container>
+    </StyledContainer>
   );
 };
 
