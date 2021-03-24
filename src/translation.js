bee1a92a760e78357da304f2e33ef4c8abcaddca
David Morais <davidmorais92@gmail.com>
2021-03-24 00:38:13 +0000

    🐛 FIX: ITA translation

--- src/translation.js
+++ src/translation.js
@@ -5,7 +5,7 @@ import XHR from "i18next-xhr-backend";
 import translationEn from "./locales/en/translation.json";
 import translationEs from "./locales/es/translation.json";
 import translationPt from "./locales/pt/translation.json";
-import translationIta from "./locales/en/translation.json";
+import translationIta from "./locales/it/translation.json";
 i18n
   .use(XHR)
   .use(LanguageDetector)
