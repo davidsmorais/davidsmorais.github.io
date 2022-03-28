a38022cd76b8ec1b01e70c904f187d5d214b019f
David Morais <davidmorais92@gmail.com>
2022-03-28 22:14:37 +0100

    update contact information

--- src/config/index.js
+++ src/config/index.js
@@ -16,7 +16,7 @@ export default {
   ],
   contacts: {
     phone: "+351 934 645 727",
-    email: "david@dsmorais.com",
+    email: "david@davidmorais.com",
   },
   socials: {
     twitter: "davidsmorais",
