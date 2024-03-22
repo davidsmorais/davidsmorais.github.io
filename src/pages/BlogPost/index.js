5e4eb2fe7bc7335e01d87ee8f10aa33270c4f8bc
David  Morais <david@davidmorais.com>
2024-03-22 12:08:47 +0000

    🐛fix: add fallback to post.title
        by David Morais <david@davidmorais.com>

--- src/pages/BlogPost/index.js
+++ src/pages/BlogPost/index.js
@@ -15,56 +15,57 @@ const POST_QUERY = (slug) => `{
 }`;
 
 const BlogPost = () => {
-  const { t } = useTranslate();
-  const id = location.hash.split("=")[1];
-  let history = useHistory();
+	const { t } = useTranslate();
+	const id = location.hash.split("=")[1];
+	const history = useHistory();
 
-  const { loading, data } = useQuery(POST_QUERY(id));
+	const { loading, data } = useQuery(POST_QUERY(id));
 
-  const post = data?.post;
+	const post = data?.post;
 
-  const postHasLoaded = !loading && post;
-  useEffect(() => {
-    if (postHasLoaded && post.title) {
-      window.document.title = `DS Morais > ${post.title}`;
-      const element = document.getElementById("scroll-target");
-      if (element) {
-        element.scrollIntoView({
-          behavior: "smooth",
-          block: "end",
-          inline: "nearest",
-        });
-      }
-    }
-  }, [postHasLoaded, post.title]);
+	const postHasLoaded = !loading && post;
+	useEffect(() => {
+		if (postHasLoaded && post?.title) {
+			window.document.title = `DS Morais > ${post.title}`;
+			const element = document.getElementById("scroll-target");
+			if (element) {
+				element.scrollIntoView({
+					behavior: "smooth",
+					block: "end",
+					inline: "nearest",
+				});
+			}
+		}
+	}, [postHasLoaded, post?.title]);
 
-  if (!loading && data.post === null) {
-    return (
-      <S.StyledContainer>
-        <Title color="red">{t("error")}</Title>
-        <Subtitle>{t("postNotFound")}</Subtitle>
-        <Button onClick={() => history.push("/all-posts")}>
-          {t("gotoAllPosts")}
-        </Button>
-      </S.StyledContainer>
-    );
-  }
-  console.log(data);
-  return (
-    <S.StyledContainer>
-      <div id="scroll-target" />
-      {!postHasLoaded ? (
-        <Title>{t("Loading")}</Title>
-      ) : (
-        <Col>
-          <img src={post.coverImage.url} />
-          <Title>{post.title}</Title>
-          <Subtitle>{new Date(post.publishedAt).toLocaleDateString()}</Subtitle>
-          <S.Content dangerouslySetInnerHTML={{ __html: post.content.html }} />
-        </Col>
-      )}
-    </S.StyledContainer>
-  );
+	if (!loading && data.post === null) {
+		return (
+			<S.StyledContainer>
+				<Title color="red">{t("error")}</Title>
+				<Subtitle>{t("postNotFound")}</Subtitle>
+				<Button onClick={() => history.push("/all-posts")}>
+					{t("gotoAllPosts")}
+				</Button>
+			</S.StyledContainer>
+		);
+	}
+	console.log(data);
+	return (
+		<S.StyledContainer>
+			<div id="scroll-target" />
+			{!postHasLoaded ? (
+				<Title>{t("Loading")}</Title>
+			) : (
+				<Col>
+					<img src={post.coverImage.url} alt="post cover" />
+					<Title>{post.title}</Title>
+					<Subtitle>{new Date(post.publishedAt).toLocaleDateString()}</Subtitle>
+					{/* biome-ignore lint/security/noDangerouslySetInnerHtml: it comes as html from the backend 🫠 */}
+					<S.Content dangerouslySetInnerHTML={{ __html: post.content.html }} />
+				</Col>
+			)}
+		</S.StyledContainer>
+	);
 };
 
 export default BlogPost;
